70c666964eb6f7fac4deb9dd7fddb03f6c75990e
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-08-11 01:13:58 +0900

    wip

--- public/setup.js
+++ public/setup.js
@@ -6,14 +6,15 @@ const query = {
 	div: this.isWin? "&& ^" : ";",
 	extractor: this.isWin? "tar -xf" : "unzip",
 	remover: this.isWin? "del" : "rm",
-	renameFile: this.isWin? "" : "arave-main.zip"
+	renameFile: this.isWin? "" : "arave-main.zip",
+	rename: this.isWin? "rename arave-main arave" : "mv arave-main.zip arave.zip"
 };
 
 execSync(`
 	curl -L -O https://github.com/ihasq/arave/archive/refs/heads/main.zip ${query.div}
 	${query.extractor} main.zip ${query.div}
 	${query.remover} main.zip ${query.div}
-	rename arave-main arave ${query.renameFile} ${query.div}
+	${query.rename} ${query.div}
 	cd arave ${query.div}
 	node ./scripts/build ${query.div}
 	cd ../
