c767c0911ca6aa11383b6739fbe75eaf1ffdfb5e
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-08-11 01:10:48 +0900

    wip

--- public/setup.js
+++ public/setup.js
@@ -2,16 +2,18 @@ const { execSync } = require("node:child_process");
 const { platform } = require("node:os");
 
 const query = {
-	div: (platform() === "win32")? "&& ^" : ";",
-	extractor: (platform() === "win32")? "tar -xf" : "unzip",
-	remover: (platform() === "win32")? "del" : "rm"
+	isWin: (platform() === "win32"),
+	div: this.isWin? "&& ^" : ";",
+	extractor: this.isWin? "tar -xf" : "unzip",
+	remover: this.isWin? "del" : "rm",
+	renameFile: this.isWin? "" : "arave-main.zip"
 };
 
 execSync(`
 	curl -L -O https://github.com/ihasq/arave/archive/refs/heads/main.zip ${query.div}
 	${query.extractor} main.zip ${query.div}
 	${query.remover} main.zip ${query.div}
-	rename arave-main arave ${query.div}
+	rename arave-main arave ${query.renameFile} ${query.div}
 	cd arave ${query.div}
 	node ./scripts/build ${query.div}
 	cd ../
