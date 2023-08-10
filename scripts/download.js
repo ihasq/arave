47b3a6dd12e6e0785ebbd3ba67da8cb2cd565030
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-08-11 00:28:16 +0900

    wip

--- scripts/download.js
+++ scripts/download.js
@@ -1,11 +1,12 @@
 const { execSync } = require("node:child_process");
 const { platform } = require("node:os");
 
-const cmddiv = (platform() === "windows")? "&& ^" : ";"
+const cmddiv = (platform() === "win32")? "&& ^" : ";"
 
 execSync(`
 	curl -L -O https://github.com/ihasq/arave/archive/refs/heads/main.zip ${cmddiv}
-	${(platform() === "windows")? "tar -xf" : "unzip"} main.zip ${cmddiv}
-	${(platform() === "windows")? "del" : "rm"} main.zip ${cmddiv}
-	cd arave-main
+	${(platform() === "win32")? "tar -xf" : "unzip"} main.zip ${cmddiv}
+	${(platform() === "win32")? "del" : "rm"} main.zip ${cmddiv}
+	cd arave-main ${cmddiv}
+	node ./scripts/build ${cmddiv}
 `.replace(/\n|\t/g, ""));
