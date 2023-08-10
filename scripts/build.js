90e606301a37b67c7143eb7156e855449518b766
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-08-10 22:44:09 +0900

    wip

--- scripts/build.js
+++ scripts/build.js
@@ -1,23 +1,20 @@
 const { execSync } = require("child_process");
 const { platform, arch } = require("os");
-
-execSync(`
-	npx esbuild ./src/main.js --bundle --minify --outdir=build --platform=node;
-	npx pkg . --target node18-${
-		(platform => {
-			switch(platform) {
-				case "darwin":	return "macos";
-				case "win32":	return "win";
-				default:		return platform;
-			}
-		})(platform())
-	}-${
-		(arch => {
-			switch(arch) {
-				case "x64":		return arch;
-				case "arm64":	return arch;
-				default:		return;
-			}
-		})(arch())
-	}
-`.replace(/\n|\t/g, ""));
+execSync(`npx esbuild ./src/main.js --bundle --minify --outfile=build/arave.js --platform=node`)
+execSync(`npx pkg . --target node18-${
+	(platform => {
+		switch(platform) {
+			case "darwin":	return "macos";
+			case "win32":	return "win";
+			default:		return platform;
+		}
+	})(platform())
+}-${
+	(arch => {
+		switch(arch) {
+			case "x64":		return arch;
+			case "arm64":	return arch;
+			default:		return;
+		}
+	})(arch())
+}`);
