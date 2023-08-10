22c5bcf0dbf561957e538d2f3d3d56a0750177e4
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-08-10 14:10:28 +0900

    wip

--- scripts/install.js
+++ scripts/install.js
@@ -5,7 +5,10 @@ execSync(`${
 	((platform) => {
 		switch(platform) {
 			case "win32":	return ""
-			default:		return "sudo cp ./build/arave /usr/local/bin/arave"
+			default:		return `
+				sudo cp ./build/arave /usr/local/bin/arave;
+				mkdir $HOME/.arave;
+			`
 		}
 	})(platform())
 }`.replace(/\n|\t/g, ""))
