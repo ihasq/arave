d44c18f73f7a00d564932c31fa07f814f73beca2
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-08-11 15:56:13 +0900

    wip

--- src/arave/term/lib.js
+++ src/arave/term/lib.js
@@ -1,14 +1,13 @@
 // import settings from "../../../arave.config.js"
 
 export const term = {
-	showAlternate: Buffer.from("\x1b[?1049h"),
-	hideAlternate: Buffer.from("\x1b[?1049l"),
-	enableBeamCursor: Buffer.from(`\x1b[6 q`),
-	disableBeamCursor: Buffer.from("\x1b[0 q"),
-	getLocator: Buffer.from([0x1b, 0x5b, ]),
-	clear: Buffer.from("\x1b[2"),
-	clearAndRepos: Buffer.from([0x1b, 0x5b, 0x32, 0x4a, 0x1b, 0x5b, 0x30, 0x3b, 0x30, 0x48]),
+	showAlternate: "\x1b[?1049h",
+	hideAlternate: "\x1b[?1049l",
+	enableBeamCursor: "\x1b[6 q",
+	disableBeamCursor: "\x1b[0 q",
+	clear: "\x1b[2J",
 	cursor: {
-		reset: Buffer.from([0x1b, 0x5b, 0x30, 0x3b, 0x30, 0x48]),
-	}
+		moveTo: (line, column) => `\x1b[${line};${column}H`,
+	},
+	report: "\x1b[0c"
 }
--- src/main.js
+++ src/main.js
@@ -4,20 +4,15 @@ import { ARAVE } from "./arave/lib.js"
 
 import process from "node:process"
 import tty from "node:tty"
+import { format } from "node:util"
 
 if(!process.stdout.isTTY && (!!tty) && (!!process)) {
 	process.exit(0)
 }
-const test = new ARAVE.TextBuffer({
-	out: process.stdout
-})
 
 process.stdin.setRawMode(true);
 process.stdin.setEncoding('utf8');
-process.stdout.write(ARAVE.term.showAlternate);
-process.stdout.write(ARAVE.term.enableBeamCursor);
-process.stdout.write(ARAVE.term.clear);
-process.stdout.write(ARAVE.term.cursor.reset);
+process.stdout.write(format(ARAVE.term.showAlternate + ARAVE.term.clear + ARAVE.term.cursor.moveTo(0,0) + ARAVE.term.enableBeamCursor));
 // ARAVE.setup.readcfg();
 
 process.stdin.on('data', key => {
@@ -26,13 +21,12 @@ process.stdin.on('data', key => {
 		case "\x03":
 			console.log('Exit process');
 
-			process.stdout.write(ARAVE.term.hideAlternate);
-			process.stdout.write(ARAVE.term.disableBeamCursor);
+			process.stdout.write(format(ARAVE.term.hideAlternate + ARAVE.term.disableBeamCursor));
 
 			process.exit(0);
 
 		case "\r":
-			process.stdout.write(ARAVE.term.clearAndRepos);
+			process.stdout.write(format(ARAVE.term.clear));
 
 		// backspace
 		case "\x7F":
