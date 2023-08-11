a4f113557f0cc6d1e8e864db654cab2fbe16f6b9
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-08-11 16:02:51 +0900

    wip

--- src/main.js
+++ src/main.js
@@ -2,38 +2,38 @@
 
 import { ARAVE } from "./arave/lib.js"
 
-import process from "node:process"
+import { stdin, stdout, exit } from "node:process"
 import tty from "node:tty"
 import { format } from "node:util"
 
-if(!process.stdout.isTTY && (!!tty) && (!!process)) {
-	process.exit(0)
+if(!stdout.isTTY && (!!tty) && (!!process)) {
+	exit(0)
 }
 
-process.stdin.setRawMode(true);
-process.stdin.setEncoding('utf8');
-process.stdout.write(format(ARAVE.term.showAlternate + ARAVE.term.clear + ARAVE.term.cursor.moveTo(0,0) + ARAVE.term.enableBeamCursor));
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+stdout.write(format(ARAVE.term.showAlternate + ARAVE.term.clear + ARAVE.term.cursor.moveTo(0,0) + ARAVE.term.enableBeamCursor));
 // ARAVE.setup.readcfg();
 
-process.stdin.on('data', key => {
+stdin.on('data', key => {
 
 	switch(key) {
 		case "\x03":
 			console.log('Exit process');
 
-			process.stdout.write(format(ARAVE.term.hideAlternate + ARAVE.term.disableBeamCursor));
+			stdout.write(format(ARAVE.term.hideAlternate + ARAVE.term.disableBeamCursor));
 
-			process.exit(0);
+			exit(0);
 
 		case "\r":
-			process.stdout.write(format(ARAVE.term.clear));
+			stdout.write(format(ARAVE.term.clear));
 
 		// backspace
 		case "\x7F":
 			break;
 
 		default: 
-			process.stdout.write(key);
+			stdout.write(key);
 	}
 
 });
