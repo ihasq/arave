1103ccd0363b049a3f400cb2b03d27f6bc8cca5a
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-08-11 22:56:14 +0900

    [ts-migrate][src] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@
 import { ARAVE } from "./arave/lib.js"
 
 import { stdin, stdout, exit } from "node:process"
-import tty from "node:tty"
 import { format } from "node:util"
 
-if(!stdout.isTTY && (!!tty) && (!!process)) {
+import tty from "node:tty"
+
+if(!stdout.isTTY) {
 	exit(0)
 }
 
@@ -15,21 +16,21 @@ stdin.setEncoding('utf8');
 stdout.write(format(ARAVE.term.showAlternate + ARAVE.term.clear + ARAVE.term.cursor.moveTo(0, 0) + ARAVE.term.enableBeamCursor));
 // ARAVE.setup.readcfg();
 
+const keyFn = {
+	"\x03"() {
+		stdout.write(format(ARAVE.term.hideAlternate + ARAVE.term.disableBeamCursor));
+		exit(0);
+	},
+	"\r"() {
+		stdout.write(format(ARAVE.term.clear + ARAVE.term.cursor.moveTo(0, 0)));
+	},
+	"\x7F"() {}
+}
+
 stdin.on('data', key => {
-
-	switch(key) {
-		case "\x03":
-			stdout.write(format(ARAVE.term.hideAlternate + ARAVE.term.disableBeamCursor));
-			exit(0);
-		case "\r":
-			stdout.write(format(ARAVE.term.clear + ARAVE.term.cursor.moveTo(0, 0)));
-			break;
-		case "\x7F":
-			// this is a backspace
-			break;
-
-		default: 
-			stdout.write(key);
-	}
-
+	if(!keyFn[key]) {
+		stdout.write(key);
+	} else {
+		keyFn[key]();
+	};
 });
