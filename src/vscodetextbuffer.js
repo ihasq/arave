7c047265707a8543d848a25b92b9fe632fbdf013
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-10-15 13:00:01 +0900

    wip

--- src/vscodetextbuffer.js
+++
@@ -1,5 +0,0 @@
-import { PieceTreeTextBufferBuilder, DefaultEndOfLine } from "./node_modules/@vscode/textbuffer/src/index"
-
-export const VSCodeTextBuffer = {
-	PieceTreeTextBufferBuilder, DefaultEndOfLine
-}
